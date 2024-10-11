0eaf99e930d83e19690a78c30112af405b7bb00d
YasodhaHeshan <yasodhaheshan2002@gmail.com>
2024-10-11 23:23:05 +0530

    create two_factor.dart UI

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ import 'package:flutter/material.dart';
 //import 'package:next_step/settings_ui.dart';
 //import 'package:next_step/set_change_password.dart';
 //import 'package:next_step/nav_bar.dart';
-import 'package:next_step/set_language_selection.dart';
+//import 'package:next_step/set_language_selection.dart';
+import 'package:next_step/two_factor.dart';
 
 
 void main() {
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LanguageSelectionScreen(), // Use the imported widget
+      home: const TwoFactorAuthScreen(), // Use the imported widget
     );
   }
 }
