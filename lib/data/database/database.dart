d30e1d80c2151e07c8a0fc5983be5b5d11123c44
Adithya Ekanayaka <130180324+AdithyaSean@users.noreply.github.com>
2024-12-15 18:47:42 +0530

    Update database.dart

--- lib/data/database/database.dart
+++ lib/data/database/database.dart
@@ -37,17 +37,14 @@ class Students extends Table {
   TextColumn get school => text().nullable()();
   TextColumn get district => text()();
   TextColumn get password => text()();
-  TextColumn get olResults =>
-      text().map(const MapConverter<String, String>())();
-  TextColumn get alResults =>
-      text().map(const MapConverter<String, String>())();
+  TextColumn get olResults => text().map(const MapConverter<String, String>())();
+  TextColumn get alResults => text().map(const MapConverter<String, String>())();
   TextColumn get stream => text().nullable()();
   RealColumn get zScore => real().nullable()();
   TextColumn get interests => text().map(const ListConverter<String>())();
   TextColumn get skills => text().map(const ListConverter<String>())();
   TextColumn get strengths => text().map(const ListConverter<String>())();
-  TextColumn get predictions =>
-      text().map(const ListConverter<Map<String, dynamic>>())();
+  TextColumn get predictions => text().map(const ListConverter<Map<String, dynamic>>())();
 
   @override
   Set<Column> get primaryKey => {id};
@@ -57,12 +54,9 @@ class StudyStreams extends Table {
   TextColumn get id => text()();
   TextColumn get name => text()();
   TextColumn get description => text()();
-  TextColumn get requiredOLSubjects =>
-      text().map(const ListConverter<String>())();
-  TextColumn get minimumOLGrades =>
-      text().map(const MapConverter<String, String>())();
-  TextColumn get possibleCourses =>
-      text().map(const ListConverter<Map<String, dynamic>>())();
+  TextColumn get requiredOLSubjects => text().map(const ListConverter<String>())();
+  TextColumn get minimumOLGrades => text().map(const MapConverter<String, String>())();
+  TextColumn get possibleCourses => text().map(const ListConverter<Map<String, dynamic>>())();
   TextColumn get relatedCareers => text().map(const ListConverter<String>())();
 
   @override
@@ -75,11 +69,9 @@ class Courses extends Table {
   TextColumn get description => text()();
   TextColumn get duration => text()();
   TextColumn get streamId => text()();
-  TextColumn get minimumALGrades =>
-      text().map(const MapConverter<String, String>())();
+  TextColumn get minimumALGrades => text().map(const MapConverter<String, String>())();
   RealColumn get minimumZScore => real()();
-  TextColumn get offeredByInstitutions =>
-      text().map(const ListConverter<String>())();
+  TextColumn get offeredByInstitutions => text().map(const ListConverter<String>())();
   TextColumn get relatedCareers => text().map(const ListConverter<String>())();
 
   @override
@@ -93,41 +85,23 @@ class AppDatabase extends _$AppDatabase {
   @override
   int get schemaVersion => 1;
 
-  // Singleton instance
-  static final AppDatabase _instance = AppDatabase._internal();
-
-  AppDatabase._internal() : super(_openConnection());
-
-  static AppDatabase get instance => _instance;
-
   // Student operations
-  Future<Student?> getStudent(String id) =>
-      (select(students)..where((t) => t.id.equals(id))).getSingleOrNull();
+  Future<Student?> getStudent(String id) => (select(students)..where((t) => t.id.equals(id))).getSingleOrNull();
   Future<void> insertStudent(Student student) => into(students).insert(student);
-  Future<void> updateStudent(Student student) =>
-      (update(students)..where((t) => t.id.equals(student.id))).write(student);
-  Future<void> deleteStudent(String id) =>
-      (delete(students)..where((t) => t.id.equals(id))).go();
+  Future<void> updateStudent(Student student) => (update(students)..where((t) => t.id.equals(student.id))).write(student);
+  Future<void> deleteStudent(String id) => (delete(students)..where((t) => t.id.equals(id))).go();
 
   // Stream operations
-  Future<StudyStream?> getStream(String id) =>
-      (select(studyStreams)..where((t) => t.id.equals(id))).getSingleOrNull();
-  Future<void> insertStream(StudyStream stream) =>
-      into(studyStreams).insert(stream);
-  Future<void> updateStream(StudyStream stream) =>
-      (update(studyStreams)..where((t) => t.id.equals(stream.id)))
-          .write(stream);
-  Future<void> deleteStream(String id) =>
-      (delete(studyStreams)..where((t) => t.id.equals(id))).go();
+  Future<StudyStream?> getStream(String id) => (select(studyStreams)..where((t) => t.id.equals(id))).getSingleOrNull();
+  Future<void> insertStream(StudyStream stream) => into(studyStreams).insert(stream);
+  Future<void> updateStream(StudyStream stream) => (update(studyStreams)..where((t) => t.id.equals(stream.id))).write(stream);
+  Future<void> deleteStream(String id) => (delete(studyStreams)..where((t) => t.id.equals(id))).go();
 
   // Course operations
-  Future<Course?> getCourse(String id) =>
-      (select(courses)..where((t) => t.id.equals(id))).getSingleOrNull();
+  Future<Course?> getCourse(String id) => (select(courses)..where((t) => t.id.equals(id))).getSingleOrNull();
   Future<void> insertCourse(Course course) => into(courses).insert(course);
-  Future<void> updateCourse(Course course) =>
-      (update(courses)..where((t) => t.id.equals(course.id))).write(course);
-  Future<void> deleteCourse(String id) =>
-      (delete(courses)..where((t) => t.id.equals(id))).go();
+  Future<void> updateCourse(Course course) => (update(courses)..where((t) => t.id.equals(course.id))).write(course);
+  Future<void> deleteCourse(String id) => (delete(courses)..where((t) => t.id.equals(id))).go();
 }
 
 LazyDatabase _openConnection() {
